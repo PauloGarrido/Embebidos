b1afbfb5bd72f10cccee4fcd6b542862a1dceca4
PauloGarrido <44560515+PauloGarrido@users.noreply.github.com>
2018-12-14 19:28:28 -0500

    Update GarridoPaulo_AlgoritmoSeleccionDatos.ino

--- GarridoPaulo_AlgoritmoSeleccionDatos.ino
+++ GarridoPaulo_AlgoritmoSeleccionDatos.ino
@@ -1,92 +1,96 @@
 #include "datos_entrenamiento.h"
 
-float suma = 0;
-int col = 0, col_n = 0; //Variable de recorrido de columnas
+float suma = 0, rend=0, acier=0;
+int col = 0; //Variable de recorrido de columnas
 int fil = 0, fil_n = 0; //Variable de recorrido de filas
 int on = 0;
 int cont = 0;
+int etiq = 0;
+int aux = 0;
 int i = 0; //Variable auxiliar
-int j=0; //Variabel auxiliar
-float matriz_n [30][5];
-float prom1 = 0, prom2 = 0, prom3 = 0, prom4 = 0; //Variable de promedio
-float dist1 = 0, dist2 = 0, dist3 = 0;
-float ran11 = 0.1, ran12 = 0.13, ran21 = 0.15, ran22 = 0.2, ran31 = 0.18, ran32 = 0.21;
+int j = 0; //Variabel auxiliar
+float matriz_u[] = {};
+float matriz_n [] = {};
+float matriz_p[30][5];
+float matriz_nueva[30][5];
+float divis = 0;
+float prom1 = 0; //Variable de promedio
+float dist1 = 0;
+float dist_mayor = 0;
 float dist_total = 2000;
 float total = 0;
-float d1 = 0, d2 = 0, d3 = 0;
+float result = 0;
+
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+
+}
+
+float knn_datos(float lim1, float lim2) {
   if (on == 0) {
-    for (; col < 4; col++) {
-      for (fil = 0; fil < 40; fil++) {
-        suma = suma + matriz[fil][col];
-        cont++;
+    for (; etiq < 4; etiq++) {
+      for (; col < 4; col++) {
+        for (fil = 0; fil < 40 * etiq; fil++) {
+          suma = suma + matriz[fil][col];
+          cont++;
+        }
+        prom1 = suma / cont;
+        matriz_p[etiq - 1][col] = prom1;
+        suma = 0;
+        fil = 0;
       }
-      prom1 = suma / cont;
-      matriz_n[0][col] = prom1;
-      suma = 0;
-      cont = 0;
+      aux = 40 * etiq;
+      col = 0;
     }
-    for (fil = 0; fil < 40; fil++) {
-      dist1 = sqrt(pow(matriz[fil][0] - matriz_n[0][col_n], 2) +
-                   pow(matriz[fil][1] - matriz_n[0][col_n], 2) +
-                   pow(matriz[fil][2] - matriz_n[0][col_n], 2) +
-                   pow(matriz[fil][3] - matriz_n[0][col_n], 2)) ;
-    }
-    if (dist_total > dist1) {
-      dist_total = dist1;
-    }
-    fil = 0;
-    col = 0;
-    dist_total = 2000;
-    for (; col < 4; col++) {
-      for (fil = 40; fil < 80; fil++) {
-        suma = suma + matriz[fil][col];
-        cont++;
+    aux = 0;
+    for (etiq = 0; etiq < 4; etiq++) {
+      for (fil = 0; fil < 40 * etiq; fil++) {
+        for (; col < 4; col++) {
+          dist1 = dist1 + pow((matriz[fil][col] - matriz_p[etiq - 1][col]), 2);
+        }
+        dist_total = sqrt(dist1);
+        matriz_n[fil] = dist_total;
+        if (dist_total > dist_mayor) {
+          dist_mayor = dist_total;
+          matriz_u[etiq - 1] = dist_mayor;
+        }
+        col = 0;
+        dist_total = 2000;
+        dist1 = 0;
       }
-      prom2 = suma / cont;
-      matriz_n[1][col] = prom2;
-      suma = 0;
-      cont = 0;
+      dist_mayor = 0;
+      aux = 40 * etiq;
     }
-    for (fil = 40; fil < 80; fil++) {
-      dist2 = sqrt(pow(matriz[fil][0] - matriz_n[1][col_n], 2) +
-                   pow(matriz[fil][1] - matriz_n[1][col_n], 2) +
-                   pow(matriz[fil][2] - matriz_n[1][col_n], 2) +
-                   pow(matriz[fil][3] - matriz_n[1][col_n], 2)) ;
-    }
-    if (dist_total > dist2) {
-      dist_total = dist2;
-    }
-    fil = 0;
-    col = 0;
-    dist_total = 2000;
-    for (; col < 4; col++) {
-      for (fil = 80; fil < 120; fil++) {
-        suma = suma + matriz[fil][col];
-        cont++;
+    for (etiq = 0; etiq < 4; etiq++) {
+      for (fil = 0; fil < 40 * etiq; fil++) {
+        for (col = 0; col < 5; col++) {
+          divis = matriz_n[fil] / matriz_u[etiq - 1];
+          if ((divis <= lim1) & (divis >= lim2))
+          {
+            matriz_nueva[fil_n][col] = matriz[fil][col];
+          }
+          divis = 0;
+        }
       }
-      prom3 = suma / cont;
-      matriz_n[2][col] = prom3;
-      suma = 0;
-      cont = 0;
+      for (fil = 0; fil < 30; fil++) {
+        for (col = 0; col < 5; col++) {
+          Serial.print(matriz_nueva[fil][col]);
+          Serial.print(",");
+        }
+      }
     }
-    for (fil = 80; fil < 120; fil++) {
-      dist3 = sqrt(pow(matriz[fil][0] - matriz_n[2][col_n], 2) +
-                   pow(matriz[fil][1] - matriz_n[2][col_n], 2) +
-                   pow(matriz[fil][2] - matriz_n[2][col_n], 2) +
-                   pow(matriz[fil][3] - matriz_n[2][col_n], 2)) ;
+    for (fil = 0; fil < 30; fil++) {
+      for (col = 0; col < 5; col++)
+        if (result == matriz_nueva[fil][col])
+          acier++;
     }
-    if (dist_total > dist3) {
-      dist_total = dist3;
-    }
-    fil = 0;
-    col = 0;
-    dist_total = 2000;
+    rend = (acier * 100) / 30;
+    Serial.println(acier);
+    Serial.println(rend);
   }
   on = 1;
 }
